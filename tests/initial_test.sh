b7a728b828f92c31b125f0a505277a406a4170b6
Tom Dowrick <tdowrick@gmail.com>
2017-01-16 17:43:23 +0000

    Changed test to use file sizes, rather than hashing due to round errors throwing off md5 hash

--- tests/initial_test.sh
+++ tests/initial_test.sh
@@ -1,5 +1,9 @@
 # Simple regression test for forward solver
 # Should only be run using default settings, when first installed
+#
+# Currently testing by confirming generated file sizes are the same.
+# This is a fairly crude method, but md5 hashing won't work, due to slightly different (at >10 decimal places)
+# voltages/jacobians being generated on different processers.
 
 # Empty output directory
 cd ../output
@@ -11,20 +15,28 @@ mpirun -np 2 ./dune_peits
 
 cd ../output
 
-# Calcuate hash of computed electrode voltage file
-md5hash=$(md5sum electrode*)
-md5hash=$(echo $md5hash | cut -f1 -d ' ')
+# Get file size of computed electrode voltage file and jacobian matrix
+voltage_size=$(wc -c electrode*)
+voltage_size=$(echo $voltage_size | cut -f1 -d ' ')
 
-# What we expect the hash to be
-correct_hash=acdcbf9cc769dca75fc983aba6f6067a
+jac_size=$(wc -c jacob*)
+jac_size=$(echo $jac_size | cut -f1 -d ' ')
 
-echo Computed hash: $md5hash
-echo Expected hash: $correct_hash
+
+# What we expect the sizes to be
+correct_voltage_size=82
+correct_jac_size=2930338
+
+echo Computed jac_size: $jac_size
+echo Expected jac_size: $correct_jac_size
+
+echo Computed voltage_size: $voltage_size
+echo Expected voltage_size: $correct_voltage_size
 
 # Check if the two hashes are the same
-if [ "$md5hash" = "$correct_hash" ]
+if [ "$voltage_size" = "$correct_voltage_size" ] && [ "$jac_size" = "$correct_jac_size" ]
 then
-	echo Hashes match - test OK
+	echo file sizes match - test OK
 else
-	echo Hashes do not match, test failed
+	echo file sizes do not match, test failed
 fi
